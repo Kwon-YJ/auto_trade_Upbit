9aa7b33e0cc975210427fece4bee01ad5abd49a7
Kwon-YJ <kyj1435@gmail.com>
2019-09-05 20:49:59 +0900

    let's start!

---
+++ ATR.py
@@ -0,0 +1,28 @@
+def day_ago(day,ohlcv,ticker):
+    a = ticker.tail(day)
+    b = a[ohlcv]
+    c = b.head(1)
+    return c
+
+def get_TR(date,ticker):
+    a = day_ago(date,'high',ticker)
+    b = day_ago(date,'low',ticker)
+    c = a.values-b.values
+
+    aa = day_ago(date,'high',ticker)
+    bb = day_ago(date+1,'close',ticker)
+    cc = abs(aa.values-bb.values)
+
+    aaa = day_ago(date,'low',ticker)
+    bbb = day_ago(date+1,'close',ticker)
+    ccc = abs(aaa.values-bbb.values)
+
+    return max(c,cc,ccc)
+
+def get_ATR(ticker):
+    value = 0
+    for i in range(10):
+        TR = get_TR(i+2,ticker)
+        value = value + TR
+    return value/10
+
